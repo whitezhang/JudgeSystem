452e12089a11962c199a73e683e1d8c0fb9fc22c
whitezhang <whitezhangv5@gmail.com>
2016-05-29 12:43:55 +0800

    add web

---
+++ web/static/js/index.js
@@ -0,0 +1,12 @@
+var app = angular.module('myApp', []);
+app.controller('customersCtrl',
+    function($scope, $http) {
+        $http.get("http://localhost:8090/contest?cid=1&&ipaddr=default").success(function(response) {
+            $scope.problemlist = response.problemlist;
+        });
+    });
+
+app.controller('formCtrl',
+    function($scope) {
+
+    });
